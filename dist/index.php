23901d310a025352095a68c430a632a3d010f883
alex_cornejo <pixelartecl@gmail.com>
2016-04-03 11:16:46 -0700

    ajuste

--- dist/index.php
+++ dist/index.php
@@ -14,7 +14,6 @@
    <canvas id="canvas" width="900px" height="600px" style="background-color: red"></canvas>
 
 <script src="assets/js/libs/utilsPixelarteGame.min.js"></script>
-<script src="assets/js/pixelarteGame.min.js"></script>
 <script src="assets/js/main.min.js"></script>
 
 
